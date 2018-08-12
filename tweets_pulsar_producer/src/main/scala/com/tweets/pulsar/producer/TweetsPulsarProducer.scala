1abd4d2d4e1781abb656926ff596c3307c3e01a1
Abhishek Sharma <abhisheksharma@Abhisheks-MacBook-Pro.local>
2018-08-12 12:52:30 -0400

    Updated tweet publishing code for one producer per executor

--- tweets_pulsar_producer/src/main/scala/com/tweets/pulsar/producer/TweetsPulsarProducer.scala
+++ tweets_pulsar_producer/src/main/scala/com/tweets/pulsar/producer/TweetsPulsarProducer.scala
@@ -2,10 +2,13 @@ package com.tweets.pulsar.producer
 
 import com.tweets.util.Util
 import org.apache.pulsar.client.api.{Producer, PulsarClient}
+import org.apache.spark.broadcast.Broadcast
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.streaming.{StreamingContext, _}
 import org.apache.spark.streaming.twitter._
 
+@deprecated
+//creating producer for each rdd partition (requirement is to have one pulsar producer per executor)
 class PuslarClientWrapper(pulsarUrl: String, topic: String) extends Serializable {
 
   @transient lazy private val client: PulsarClient = PulsarClient.builder.serviceUrl(pulsarUrl).build
@@ -22,6 +25,30 @@ class PuslarClientWrapper(pulsarUrl: String, topic: String) extends Serializable
   }
 }
 
+// added wrapper class for pulsar producer, so that per producer per executor will publish tweets.
+class PuslarSink(createProducer: () => Producer[Array[Byte]] ) extends Serializable {
+  @transient lazy private val producer = createProducer()
+  def send(value: String): Unit = producer.send(value.getBytes)
+}
+
+object PuslarSink {
+  def apply(config: Map[String, String]): PuslarSink = {
+    val f = () => {
+      val client: PulsarClient = PulsarClient.builder.serviceUrl(config("pulsarUrl")).build
+      val producer: Producer[Array[Byte]] = client.newProducer.topic(config("topic")).create
+
+      sys.addShutdownHook {
+        producer.flush()
+        producer.close()
+        client.close()
+      }
+
+      producer
+    }
+    new PuslarSink(f)
+  }
+}
+
 class TweetsPulsarProducer(args: Array[String]) {
   def run(): Unit = {
     //setting up spark-streaming context
@@ -30,16 +57,18 @@ class TweetsPulsarProducer(args: Array[String]) {
     val streamingContext = new StreamingContext(sparkSession.sparkContext, duration)
 
     //setting up pulsar client & topic producer
-    val producer = new PuslarClientWrapper( args(0), args(1))
-    publishTweets(streamingContext, producer, args(2))
+    //val producer = new PuslarClientWrapper( args(0), args(1))
+    val conf: Map[String, String] = Map("pulsarUrl" -> args(0), "topic" -> args(1))
+    val pulsarSink: Broadcast[PuslarSink] = sparkSession.sparkContext.broadcast(PuslarSink(conf))
+    publishTweets(streamingContext, pulsarSink, args(2))
 
     // starting tweets publishing to pulsar
     streamingContext.start
     streamingContext.awaitTermination
-    producer.close()
+    //producer.close()
   }
 
-  def publishTweets(streamingContext: StreamingContext, producer: PuslarClientWrapper, tweetsFilter: String): Unit  = {
+  def publishTweets(streamingContext: StreamingContext, pulsarSink: Broadcast[PuslarSink], tweetsFilter: String): Unit  = {
     val stream = TwitterUtils.createStream(streamingContext, None)
     val filteredTweets = stream.filter(status => status.getText.split(" ")
       .toSet.exists(str => str.contains(tweetsFilter))).map(status => status.getText.replace('\n', ' '))
@@ -47,7 +76,7 @@ class TweetsPulsarProducer(args: Array[String]) {
     filteredTweets.foreachRDD {
       rdd => rdd.foreachPartition {
         iterator => iterator.foreach {
-          tweet => producer.send(tweet)
+          tweet => pulsarSink.value.send(tweet)
         }
       }
     }
